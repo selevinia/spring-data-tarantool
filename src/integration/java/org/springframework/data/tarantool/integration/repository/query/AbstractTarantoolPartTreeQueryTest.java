9aa89ed5e1265e1d42505f08fdfa43c530f43a42
rx-alex <arublev@phoenixit.ru>
2021-08-04 16:58:37 +0300

    Implement non reactive Tarantool repository with PartTree query tests

--- src/integration/java/org/springframework/data/tarantool/integration/repository/query/AbstractTarantoolPartTreeQueryTest.java
+++ src/integration/java/org/springframework/data/tarantool/integration/repository/query/AbstractTarantoolPartTreeQueryTest.java
@@ -4,6 +4,7 @@ import io.tarantool.driver.api.conditions.Conditions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.data.tarantool.core.TarantoolOperations;
 import org.springframework.data.tarantool.integration.domain.User;
 import org.springframework.data.tarantool.repository.TarantoolRepository;
@@ -13,6 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.springframework.data.tarantool.integration.repository.util.TestData.newUser;
 
 /**
@@ -47,6 +49,110 @@ public abstract class AbstractTarantoolPartTreeQueryTest {
     @Test
     void shouldNotFindAllByLastName() {
         List<User> notFound = userRepository.findAllByLastName("Unused");
+        assertThat(notFound).isEmpty();
+    }
+
+    @Test
+    void shouldFindAllByBirthDate() {
+        List<User> found = userRepository.findAllByBirthDate(LocalDate.now().minusYears(21));
+        assertThat(found).hasSize(2);
+    }
+
+    @Test
+    void shouldFindAllByFirstNameAndLastName() {
+        List<User> found = userRepository.findAllByFirstNameAndLastName("Alexey", "Kuzin");
+        assertThat(found).hasSize(4);
+    }
+
+    @Test
+    void shouldFindTop2ByFirstNameAndLastName() {
+        List<User> found = userRepository.findTop2ByFirstNameAndLastName("Alexey", "Kuzin");
+        assertThat(found).hasSize(2);
+    }
+
+    @Test
+    void shouldFindOneByFirstNameAndLastName() {
+        User found = userRepository.findByFirstNameAndLastName("Vasiliy", "Pupkin");
+        assertThat(found).isNotNull();
+    }
+
+    @Test
+    void shouldNotFindAllByAge() {
+        assertThatThrownBy(() -> userRepository.findAllByAge("21"))
+                .isInstanceOf(InvalidDataAccessApiUsageException.class);
+    }
+
+    @Test
+    void shouldFindFirst2ByAge() {
+        List<User> found = userRepository.findFirstByAge(21);
+        assertThat(found).hasSize(1);
+    }
+
+    @Test
+    void shouldFindAllByAgeLessThan() {
+        List<User> found = userRepository.findAllByAgeLessThan(22);
+        assertThat(found).hasSize(3);
+    }
+
+    @Test
+    void shouldFindAllByAgeLessThanEqual() {
+        List<User> found = userRepository.findAllByAgeLessThanEqual(22);
+        assertThat(found).hasSize(4);
+    }
+
+    @Test
+    void shouldFindAllByAgeGreaterThan() {
+        List<User> found = userRepository.findAllByAgeGreaterThan(22);
+        assertThat(found).hasSize(1);
+    }
+
+    @Test
+    void shouldFindAllByAgeGreaterThanEqual() {
+        List<User> found = userRepository.findAllByAgeGreaterThanEqual(22);
+        assertThat(found).hasSize(2);
+    }
+
+    @Test
+    void shouldFindAllByAgeAfter() {
+        List<User> found = userRepository.findAllByAgeAfter(22);
+        assertThat(found).hasSize(1);
+    }
+
+    @Test
+    void shouldFindAllByAgeBefore() {
+        List<User> found = userRepository.findAllByAgeBefore(22);
+        assertThat(found).hasSize(3);
+    }
+
+    @Test
+    void shouldFindAllByActiveTrue() {
+        List<User> found = userRepository.findAllByActiveTrue();
+        assertThat(found).hasSize(4);
+    }
+
+    @Test
+    void shouldFindAllByActiveFalse() {
+        List<User> found = userRepository.findAllByActiveFalse();
+        assertThat(found).hasSize(1);
+    }
+
+    @Test
+    void shouldCountByLastName() {
+        Long count = userRepository.countByLastName("Kuzin");
+        assertThat(count).isEqualTo(4L);
+    }
+
+    @Test
+    void shouldExistsByLastName() {
+        Boolean exists = userRepository.existsByLastName("Kuzin");
+        assertThat(exists).isTrue();
+    }
+
+    @Test
+    void shouldDeleteByLastName() {
+        userRepository.deleteByLastName("Kuzin");
+
+        List<User> notFound = userRepository.findAllByLastName("Kuzin");
         assertThat(notFound).isEmpty();
     }
 
